48639c5e3676529a6e5fa09b50bc1dd9f0df6bf3
aaron-cole <aaron-cole@woh.rr.com>
2021-03-25 11:27:33 -0400

    Update V-214801.sh
    reflect new version of McAfee ENSL

--- STIGS/RHEL7/V-214801.sh
+++ STIGS/RHEL7/V-214801.sh
@@ -32,6 +32,15 @@ if rpm -q ISecTP >> $Results; then
   echo "McAfee ENSL Installed but not running" >> $Results  
   echo "Fail" >> $Results
  fi
+elif rpm -q McAfeeTP >> $Results; then
+ echo "McAfee ENSL Status - $(systemctl state mfetpd 2>> $Results)" >> $Results
+ if [ "$(systemctl is-enabled mfetpd 2>>/dev/null)" == "enabled" ] && [ "$(systemctl is-active mfetpd 2>>/dev/null)" == "active" ]; then
+  /opt/McAfee/ens/tp/bin/mfetpcli --version >> $Results
+  echo "Pass" >> $Results
+ else
+  echo "McAfee ENSL Installed but not running" >> $Results  
+  echo "Fail" >> $Results
+ fi
 else
  echo "McAfee ENSL is not installed" >> $Results
  echo "Fail" >> $Results
